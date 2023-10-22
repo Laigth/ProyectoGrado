a53f52c32230e3b106f80ce7bad245caa56efd77
Raul Condori <condori.raul.800@gmail.com>
2023-10-22 09:34:13 -0400

    cambios en vista login, adecuándolo para que se vea  la contraseña

--- ProyectoFinal/app/Views/usuarios/login.php
+++ ProyectoFinal/app/Views/usuarios/login.php
@@ -28,57 +28,49 @@
               </div>
               <h3 style="font-family: Forte; text-align: center;">Veterinaria América</h3>
               <h6 class="font-weight-light" style="text-align: center">Bienvenido!</h6>
-              <form class="pt-3" action="<?= base_url('usuarios/inicioSesion') ?>" method="post">
-              <div class="form-group" id="nombre">
-              <label for="nombre_usuario">Nombre de usuario:</label>
-              <input type="text" class="form-control" name="nombre_usuario" value="<?= old('nombre_usuario') ?>" required placeholder="Usuario">
-            </div>
-            <div class="form-group">
-                <label for="contrasena">Contraseña:</label>
-                <div class="input-group">
-                    <div class="input-group-prepend bg-transparent">
-                    <span class="input-group-text bg-transparent border-right-0">
-                        <i class="mdi mdi-lock-outline text-primary"></i>
-                    </span>
-                    </div>
-                    <input type="password" class="form-control form-control-lg border-left-0" name="contrasena" id="contrasenaInput" placeholder="Contraseña" required>                        
-                    <div class="input-group-append">
-                    <button type="button" id="togglePassword" class="btn btn-sm btn-secondary input-group-text">
-                        <span id="icon" class="mdi mdi-eye"></span>
-                    </button>
-                    </div>
-                </div>
-                </div>
 
-           
-             <!--  <div class="my-2 d-flex justify-content-between align-items-center">
-                  <div class="form-check">
-                    <label class="form-check-label text-muted">
-                      <input type="checkbox" class="form-check-input">
-                      Keep me signed in
-                    </label>
-                  </div>
-                  <a href="#" class="auth-link text-black">Has olvidado tu contraseña?</a>
-                </div>  -->
-            <div class="my-3">
-              <button type="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" style="font-family: Forte; display: block; margin: auto">Iniciar sesión</button>
-            </div>
-                <div class="mb-2 d-flex">
-                  <button type="button" class="btn btn-facebook auth-form-btn flex-grow me-1">
-                    <i class="mdi mdi-facebook me-2"></i>Facebook
-                  </button>
-                  <button type="button" class="btn btn-google auth-form-btn flex-grow ml-1">
-                    <i class="mdi mdi-google me-2"></i>Google
-                  </button>
+              <?php if (session()->getFlashdata('error')) : ?>
+                <div class="alert alert-danger">
+                  <?= session()->getFlashdata('error') ?>
                 </div>
-                <div class="text-center mt-4 font-weight-light">
-                ¿No tienes una cuenta? <a href="<?= base_url('usuarios/register') ?>" class="text-primary" style="font-family: Forte; display: block; margin: auto" >Regístrate</a>
+              <?php endif; ?>
+
+              <form class="pt-3" action="<?= base_url('usuarios/inicioSesion') ?>" method="post">
+                <div class="form-group" id="nombre">
+                  <label for="nombre_usuario">Nombre de usuario:</label>
+                  <div class="input-group">
+                    <div class="input-group-prepend bg-transparent">
+                      <span class="input-group-text bg-transparent border-right-0">
+                        <i class="mdi mdi-account-outline text-primary"></i>
+                      </span>
+                    </div>
+                    <input type="text" class="form-control form-control-lg border-left-0" name="nombre_usuario" value="<?= old('nombre_usuario') ?>" required placeholder="Usuario">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="contrasena">Contraseña:</label>
+                  <div class="input-group">
+                    <div class="input-group-prepend bg-transparent">
+                      <span class="input-group-text bg-transparent border-right-0">
+                        <i class="mdi mdi-lock-outline text-primary"></i>
+                      </span>
+                    </div>
+                    <input type="password" class="form-control form-control-lg border-left-0" name="contrasena" id="contrasenaInput" placeholder="Contraseña" required>
+                    <div class="input-group-append">
+                      <button type="button" id="togglePassword" class="btn btn-sm btn-secondary input-group-text">
+                        <span id="icon" class="mdi mdi-eye"></span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div class="my-3">
+                  <button type="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" style="font-family: Forte; display: block; margin: auto">Iniciar sesión</button>
                 </div>
               </form>
             </div>
           </div>
           <div class="col-lg-6 login-half-bg d-lg-flex flex-row">
-            <p class="text-white font-weight-medium text-center flex-grow align-self-end">AnimalCareSolution-2023  All rights reserved.</p>
+            <p class="text-white font-weight-medium text-center flex-grow align-self-end">AnimalCareSolution-2023 All rights reserved.</p>
           </div>
         </div>
       </div>
@@ -88,25 +80,25 @@
   </div>
 
 
-<!-- ... (código HTML existente) ... -->
+  <!-- ... (código HTML existente) ... -->
 
-<script>
-  const togglePasswordButton = document.getElementById('togglePassword');
-  const passwordInput = document.getElementById('contrasenaInput');
-  const icon = document.getElementById('icon');
+  <script>
+    const togglePasswordButton = document.getElementById('togglePassword');
+    const passwordInput = document.getElementById('contrasenaInput');
+    const icon = document.getElementById('icon');
 
-  togglePasswordButton.addEventListener('click', function () {
-    if (passwordInput.type === 'password') {
-      passwordInput.type = 'text';
-      icon.classList.remove('mdi-eye');
-      icon.classList.add('mdi-eye-off');
-    } else {
-      passwordInput.type = 'password';
-      icon.classList.remove('mdi-eye-off');
-      icon.classList.add('mdi-eye');
-    }
-  });
-</script>
+    togglePasswordButton.addEventListener('click', function() {
+      if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        icon.classList.remove('mdi-eye');
+        icon.classList.add('mdi-eye-off');
+      } else {
+        passwordInput.type = 'password';
+        icon.classList.remove('mdi-eye-off');
+        icon.classList.add('mdi-eye');
+      }
+    });
+  </script>
 
   <!-- container-scroller -->
   <!-- base:js -->
